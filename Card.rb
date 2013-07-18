bdca36b3e5dc6abafcee08768bac62c5d8e4d33e
gf5901 <gfong@student.neumont.edu>
2013-07-18 09:21:04 -0600

    Made Card.rb

---
+++ Card.rb
@@ -0,0 +1,17 @@
+
+
+class Card
+  
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
+  end
+  
+  def get_value
+    return @value
+  end
+  
+  def get_suit
+    return @suit
+  end
+end
